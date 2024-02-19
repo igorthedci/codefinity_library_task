9afb1537ff8e05028a1fae856912d42f77adaa1e
igorthedci <foumart@ukr.net>
2024-02-19 10:39:09 +0200

    Library task is completed

--- src/main/java/codefinity/Library.java
+++ src/main/java/codefinity/Library.java
@@ -1,27 +1,54 @@
 package codefinity;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Library {
 
-    public void addBook(Book book) {
+    public List<Book> books;
 
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
     }
 
     public void removeBookById(int id) {
-
+        List<Book> removedList = new ArrayList<>();
+        for (Book aBook : books) {
+            if (aBook.getId() == id) {
+                removedList.add(aBook);
+            }
+        }
+        books.removeAll(removedList);
     }
 
     public List<Book> findBooksByAuthor(String author) {
-        return Collections.emptyList();
+        List<Book> filterList = new ArrayList<>();
+        for (Book aBook : books) {
+            if (aBook.getAuthor().equals(author)) {
+                filterList.add(aBook);
+            }
+        }
+        return filterList;
     }
 
     public List<Book> findBooksPublishedAfterYear(int year) {
-        return Collections.emptyList();
+        List<Book> filterList = new ArrayList<>();
+        for (Book aBook : books) {
+            if (aBook.getYear() > year) {
+                filterList.add(aBook);
+            }
+        }
+        return filterList;
     }
 
     public void displayAllBooks() {
 
+        System.out.print(books);
+        System.out.print('\n');
     }
 }
